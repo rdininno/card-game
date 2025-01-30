1e3e7a1ef03c8441fa56630b6b8bccb80af69044
rdininno <robertdininno@gmail.com>
2025-01-29 17:30:45 -0800

    display cards on homepage with shuffle button

---
+++ src/utils/gameLogic.js
@@ -0,0 +1,24 @@
+const suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+// Create a full deck
+export function getDeck() {
+    let deck = [];
+
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push({ suit, value });
+        }
+    }
+
+    return deck;
+}
+
+// Shuffle the deck
+export function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); 
+        [deck[i], deck[j]] = [deck[j], deck[i]]; // Swap
+    }
+    return deck;
+}
