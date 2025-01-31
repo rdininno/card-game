e59894b64cdaa9d23dee6cd54bb25f03cd7f7c99
rdininno <robertdininno@gmail.com>
2025-01-31 15:35:40 -0800

    style player

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,41 +2,44 @@
 
 import React from "react";
 import Card from "./Card";
+import "../app/player.css";
+
 
 const Player = ({ name, hand, playerIndex, isCurrentPlayer, handleCardClick, drawPileEmpty }) => {
     return (
-        <div>
-            <h2>{name} {isCurrentPlayer ? "(Your Turn)" : ""}</h2>
+        <div className="player">
+            <h2 className="player-name">{name} {isCurrentPlayer ? "(Your Turn)" : ""}</h2>
 
-            <h3>Face-Down Cards:</h3>
-            <div>
-                {hand.faceDown.map((_, i) => (
-                    <Card key={i} hidden 
+            <div className="player-hand">
+                <div className="player-cards facedown">
+                    {hand.faceDown.map((_, i) => (
+                        <Card 
+                            key={i} 
+                            hidden 
+                            onClick={() => {
+                                if (isCurrentPlayer && hand.inHand.length === 0 && hand.faceUp.length === 0 && drawPileEmpty) {
+                                    handleCardClick(playerIndex, i, false, true);
+                                }
+                            }} />
+                    ))}
+                </div>
+
+                <div className="player-cards faceup">
+                    {hand.faceUp.map((card, i) => (
+                        <Card key={i} suit={card.suit} value={card.value} 
                         onClick={() => {
-                            if (isCurrentPlayer && hand.inHand.length === 0 && hand.faceUp.length === 0 && drawPileEmpty) {
-                                handleCardClick(playerIndex, i, false, true);
+                            if (isCurrentPlayer && hand.inHand.length === 0 && drawPileEmpty) {
+                                handleCardClick(playerIndex, i, true, false);
                             }
-                        }} />
-                ))}
-            </div>
+                        }}/>
+                    ))}
+                </div>
 
-            <h3>Face-Up Cards:</h3>
-            <div >
-                {hand.faceUp.map((card, i) => (
-                    <Card key={i} suit={card.suit} value={card.value} 
-                    onClick={() => {
-                        if (isCurrentPlayer && hand.inHand.length === 0 && drawPileEmpty) {
-                            handleCardClick(playerIndex, i, true, false);
-                        }
-                    }}/>
-                ))}
-            </div>
-
-            <h3>In-Hand Cards (Click to Play):</h3>
-            <div>
-                {hand.inHand.map((card, i) => (
-                    <Card key={i} suit={card.suit} value={card.value} onClick={() => handleCardClick(playerIndex, i, false, false)} />
-                ))}
+                <div className="player-cards inhand">
+                    {hand.inHand.map((card, i) => (
+                        <Card key={i} suit={card.suit} value={card.value} onClick={() => handleCardClick(playerIndex, i, false, false)} />
+                    ))}
+                </div>
             </div>
         </div>
     );
